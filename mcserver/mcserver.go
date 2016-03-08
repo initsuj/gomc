5db48edf89513ae98e999815d3791cccb35c68a8
initsuj <initsuj@outlook.com>
2016-03-08 16:18:36 -0700

    ping returns results

--- mcserver/mcserver.go
+++ mcserver/mcserver.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	bin "github.com/initsuj/binary"
+	"io"
 	"net"
 	"strconv"
 )
@@ -12,10 +14,12 @@ const (
 	DefaultPort uint16 = 25565
 )
 
-type packet struct {
-	Length int
-	Id     uint16
-	Data   []byte
+type Handshake struct {
+	Id    int32 `bin:"varint"`
+	Proto int32 `bin:"varint"`
+	Addr  string
+	Port  uint16
+	Next  int32 `bin:"varint"`
 }
 
 func GetServerInfo(server string) error {
@@ -33,21 +37,70 @@ func GetServerInfo(server string) error {
 		port = uint16(i)
 	}
 
-	buf := new(bytes.Buffer)
+	h := &Handshake{
+		Id:    0,
+		Proto: 4,
+		Addr:  s,
+		Port:  port,
+		Next:  1,
+	}
 
-	//n := binary.PutVarint(buf, 0x00)
-	n := binary.PutVarint(buf, 0)
-	n += binary.PutVarint(buf, int64(len(s)))
-	i, err := buf.WriteString(s)
+	pkt, err := bin.Marshal(h)
 	if err != nil {
 		return err
 	}
 
-	n += i
-	n += binary.BigEndian.PutUint16(buf, port)
-	n += binary.PutVarint(buf, 1)
+	var buf bytes.Buffer
+	buf.Write(varint2b(uint64(len(pkt))))
+	buf.Write(pkt)
+	fmt.Printf("sent pkt(%v) %v\n", varint2b(uint64(len(pkt))), h)
 
-	fmt.Printf("count: %v - % x", len(buf.Bytes()), buf.Bytes())
+	c, err := net.Dial("tcp", server)
+	if err != nil {
+		return err
+	}
+
+	defer c.Close()
+	fmt.Printf("listening: %v\n", c.LocalAddr().String())
+
+	fmt.Printf("sent: %02x", buf.Bytes())
+	//buf.WriteTo(os.Stdout)
+	buf.WriteTo(c)
+
+	buf.Reset()
+
+	ping := varint2b(0)
+	buf.Write(varint2b(uint64(len(ping))))
+	buf.Write(pkt)
+
+	buf.WriteTo(c)
+
+	var b bytes.Buffer
+	io.Copy(&b, c)
+	fmt.Println("total size:", b.Len())
+
 	return nil
 
+}
+
+func varint2b(x uint64) []byte {
+	fmt.Printf("received %v\n", x)
+	b := make([]byte, binary.MaxVarintLen64)
+	n := binary.PutUvarint(b, x)
+	fmt.Printf("created %v : %v\n", n, b[:n])
+	return b[:n]
+}
+
+func ushort2b(x uint16) (b []byte) {
+	b = make([]byte, 2)
+	binary.BigEndian.PutUint16(b, x)
+
+	return
+}
+
+func str2b(s string) (b []byte) {
+	b = make([]byte, len(s))
+	b = []byte(s)
+	b = append(varint2b(uint64(len(b))), b...)
+	return
 }
--- mcserver/proto47.go
+++ mcserver/proto47.go
@@ -5,7 +5,9 @@ const ()
 type Proto47 struct {
 }
 
-func (p Proto47) PackHandshake()
+func (p Proto47) PackHandshake() {
+
+}
 
 func (p Proto47) EncodeString(s string) []byte {
 	return nil
